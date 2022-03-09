0cd35a2b402d09c0a46a504ac0e07240b1b90f8f
zhenshan <zhenshan@belmont.tech>
2022-03-09 08:45:50 -0600

    update code

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ root_folder = tempfile.gettempdir()
 nltk.download('wordnet', download_dir=root_folder)
 nltk.data.path.append(root_folder)
 
+# add code 
+
 def compute(test):
 
     return {"result": [i.name() for i in wn.synsets("same")]}
